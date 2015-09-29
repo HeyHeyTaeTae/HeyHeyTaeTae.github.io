60845a59f39c741282bd12e40462356f839e47b0
Tae Kim <taekimsf@gmail.com>
2015-09-28 19:04:45 -0700

    added setTimeout delay for image render

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-	
+	window.setTimeout(function(){
 		$("#myName").css("opacity");
 		$("#myName").addClass("load");
 		$(".link").css("opacity");
 		$(".link").addClass("load");
+	}
+		, 1);
 	
 });
