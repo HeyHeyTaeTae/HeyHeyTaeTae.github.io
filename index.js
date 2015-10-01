d456f4ad011fa37d7418c6deec89ec4116aeaeed
Tae Kim <taekimsf@gmail.com>
2015-09-30 17:53:56 -0700

    scrapped css and added media query for mobile design

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	// window.setTimeout(function(){
-		$("#myName").css("opacity"); // force browser to set opacity
-		$("#myName").addClass("load"); // change opacity
-		$(".link").css("opacity");
-		$(".link").addClass("load");
+		// $("#myName").css("opacity"); // force browser to set opacity
+		// $("#myName").addClass("load"); // change opacity
+		// $(".link").css("opacity");
+		// $(".link").addClass("load");
 	// }, 1000);
 });
