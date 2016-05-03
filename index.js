67503e8b6f46051bc0c5e1123a1c51eb5b19a29c
Tae Kim <taekim@ajp.local>
2016-05-02 20:24:17 -0700

    icons and project gallery setup

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	console.log("this works")
 
-		$(".name").hide().fadeIn(2000);
-		$(".title").hide().fadeIn(2000);
-
-
+		// $(".name").hide().fadeIn(2500);
+		$(".name-and-title").hide().fadeIn(3000);
+		// $(".linkedin-icon").hide().fadeIn(8000);
+		// $(".github-icon").hide().fadeIn(8000);
+	
 });
 
 
