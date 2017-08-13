33aa98eb364332454d2c58fc5178a40e0b3bbbb7
Tae Kim <taekimsf@gmail.com>
2017-08-12 20:10:02 -0700

    changed positioning of about content and made sections shorter

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ $(document).ready(function(){
 		$(".name-and-title").hide().fadeIn(5000);
 
 		$(".second-project").on("mouseover", function() {
-
 			var width = $(this).width();
-
-			console.log("the width of the 2nd project div is " + width + "px");
-
+			// console.log("the width of the 2nd project div is " + width + "px");
 			var tooltip = $(this).children(".tooltip");
-
-			console.log(tooltip);
-
 			tooltip.css("width", width);
-
-		})
-
+		});
 	
 });
 
