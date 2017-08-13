a7ec330b220ab7a2e44678744a4d318ff31ab522
Tae Kim <taekimsf@gmail.com>
2017-08-13 02:11:40 -0700

    created resume html and resume css files

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
 	console.log("this works")
 	
+	$(".first-project").on("mouseover", function() {
+		var width = $(this).width();
+		// console.log("the width of the 2nd project div is " + width + "px");
+		var tooltip = $(this).children(".tooltip");
+		tooltip.css("width", width);
+	});
+
 	$(".second-project").on("mouseover", function() {
+		var width = $(this).width();
+		// console.log("the width of the 2nd project div is " + width + "px");
+		var tooltip = $(this).children(".tooltip");
+		tooltip.css("width", width);
+	});
+
+	$(".third-project").on("mouseover", function() {
 		var width = $(this).width();
 		// console.log("the width of the 2nd project div is " + width + "px");
 		var tooltip = $(this).children(".tooltip");
