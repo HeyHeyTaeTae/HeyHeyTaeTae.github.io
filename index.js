69f1c5eb4158ff3bff8fff367b53924870451c6c
Tae Kim <taekimsf@gmail.com>
2017-08-12 20:56:22 -0700

    added a .7 second setTimeout for the name title fade in to account for hero img loading time on page load and increased fade-in time of name and title to 8 seconds

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
 	console.log("this works")
-
-		$(".name-and-title").hide().fadeIn(4500);
-
-		$(".second-project").on("mouseover", function() {
-			var width = $(this).width();
-			// console.log("the width of the 2nd project div is " + width + "px");
-			var tooltip = $(this).children(".tooltip");
-			tooltip.css("width", width);
-		});
 	
+	$(".second-project").on("mouseover", function() {
+		var width = $(this).width();
+		// console.log("the width of the 2nd project div is " + width + "px");
+		var tooltip = $(this).children(".tooltip");
+		tooltip.css("width", width);
+	});
+
+	setTimeout(function() {
+
+		$(".name-and-title").hide().fadeIn(8000);
+
+	}, 700);
+				
 });
+
+
 
 
 
